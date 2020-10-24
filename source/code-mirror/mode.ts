8ff7a949b693d8ebc6dfcbb851bfd71ec008a90d
Ziphil <yfe23457@nifty.com>
2020-10-24 16:32:09 +0900

    CodeMirror用シンタックスハイライトの構文定義を変更

--- source/code-mirror/mode.ts
+++ source/code-mirror/mode.ts
@@ -6,13 +6,33 @@ import "codemirror/addon/mode/simple";
 
 let CodeMirrorAny = CodeMirror as any;
 
+let base = [
+  {regex: /"([^"\\]|\\"|\\\\|\\u[A-Fa-f0-9]{0,4})*?"/, token: "string"},
+  {regex: /\/([^"\\]|\\\/|\\\\|\\u[A-Fa-f0-9]{0,4})*?\//, token: "string"},
+  {regex: /[a-zA-Z][a-zA-Z0-9_]*/, token: "variable-1"},
+  {regex: /@[a-zA-Z][a-zA-Z0-9_]*/, token: "variable-3"},
+  {regex: /(\^|\$)/, token: "operator"},
+  {regex: /#.*/, token: "comment"}
+];
+
 CodeMirrorAny.defineSimpleMode("akrantiain", {
   start: [
-    {regex: /"([^"\\]|\\"|\\\\|\\u[A-Fa-f0-9]{0,4})*?"/, token: "string"},
-    {regex: /\/([^"\\]|\\\/|\\\\|\\u[A-Fa-f0-9]{0,4})*?\//, token: "string"},
-    {regex: /[A-Za-z0-9_]+/, token: "variable-1"},
-    {regex: /@[A-Za-z0-9_]+/, token: "variable-2"},
-    {regex: /(\^|\$)/, token: "operator"},
-    {regex: /#.*/, token: "comment"}
+    {regex: /%%/, push: "moduleChain"},
+    {regex: /%(?!%)/, next: "moduleName"},
+    ...base
+  ],
+  module: [
+    {regex: /\}/, next: "start"},
+    ...base
+  ],
+  moduleName: [
+    {regex: /\{/, next: "module"},
+    {regex: /[a-zA-Z][a-zA-Z0-9_]*/, token: "variable-2"},
+    ...base
+  ],
+  moduleChain: [
+    {regex: /(?=;|#|$)/, pop: true},
+    {regex: /[a-zA-Z][a-zA-Z0-9_]*/, token: "variable-2"},
+    ...base
   ]
 });
